55cac91929b14ade008eab37a32ca0b602896b12
S25088 <S25088@pjwstk.edu.pl>
2024-03-25 22:39:56 +0100

    Modyfikacja 2

--- App/Program.cs
+++ App/Program.cs
@@ -3,5 +3,9 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Hello world");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("hello");
+        }
     }
 }
